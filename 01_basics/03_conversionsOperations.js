6ee9a9cbdeeb3813a0ff3956f918a6c7204df243
yashank123 <103108441+yashank123@users.noreply.github.com>
2024-07-18 16:31:24 +0000

    #

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -1,4 +1,4 @@
-let score = 123;
+/*let score = 123;
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -17,4 +17,26 @@ valueInNUmber = Number(score);
 console.log(typeof valueInNUmber);
 console.log(valueInNUmber); // NaN not a number
 
-// 1->true , 0->false , "abc"->true , ""->false
+// 1->true , 0->false , "abc"->true , ""->false; */ 
+
+// ************ OPERATIONS ************
+
+/*let value = 12;
+let value2 = -value;
+console.log(value);
+console.log(value2);
+
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);*/ 
+
+console.log("1" + 2);
+console.log(2 + "1");
+console.log("3" + "3");
+console.log(2 + 2 + "1");
+//if string is at first then all are treated as string
+//if string is at last then all before it are treated as number
+//if string is at middle then all after it are treated as string (including itself)
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// > , < , >= , <= , == , === , != 
+
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// === strict check , checks both value and datatype
+console.log("2" === 2);
