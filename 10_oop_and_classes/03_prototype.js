91a3d446b252a64923df453b15654cf49f5986bd
Yashank Lakhena <103108441+yashank123@users.noreply.github.com>
2024-08-28 18:18:23 +0000

    .

--- 10_oop_and_classes/03_prototype.js
+++ 10_oop_and_classes/03_prototype.js
@@ -4,3 +4,36 @@
 // prototypal behaviour -> harr nahi manna / prototypal inheritance
 
 // prototype -> functions + some internal properties
+
+let myName = "Yashank     "
+let mychannel = "chai     "
+
+console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh = function(){               // injecting prototype in objects/highest herarchy(directly injected in arrays, fun, obj)
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()         // not work as heyHitesh() prototype is for array not for objects (described for child so not work in parent but if described in parent will work for child)
+
+//********** inheritance ******************************* */
